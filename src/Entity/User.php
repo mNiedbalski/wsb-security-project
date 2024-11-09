15c9705c31f5f4e09ae7f44ac8782ac618285324
Michał Niedbalski <michnie507@student.polsl.pl>
2024-11-09 15:18:09 +0100

    Another bunch of boilerplate stuff to add doctrine orm

---
+++ src/Entity/User.php
@@ -0,0 +1,77 @@
+<?php
+
+// src/Entity/User.php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: "App\Repository\UserRepository")]
+class User implements UserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "string", unique: true)]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 5)]
+    private $username;
+
+    #[ORM\Column(type: "string")]
+    #[Assert\NotBlank]
+    private $password;
+
+    #[ORM\Column(type: "json")]
+    private $roles = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->id;
+    }
+}
